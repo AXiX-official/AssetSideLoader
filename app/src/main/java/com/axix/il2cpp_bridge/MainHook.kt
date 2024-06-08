6697022ba6e95f165c805b1566def7eb072b9615
AXiX-official <2879710747@qq.com>
2024-06-08 08:33:51 +0800

    Init

---
+++ app/src/main/java/com/axix/il2cpp_bridge/MainHook.kt
@@ -0,0 +1,18 @@
+package com.axix.il2cpp_bridge
+
+import android.util.Log
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.XposedHelpers
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+
+class MainHook : IXposedHookLoadPackage {
+    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
+        if (lpparam.packageName == "com.bilibili.azurlane") {
+            Log.i("IL2CPP_Bridge", "Loaded package: ${lpparam.packageName}")
+
+
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        jcenter()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        jcenter()
+    }
+}
+
+rootProject.name = "Il2cpp-bridge"
+include(":app")
+ 
